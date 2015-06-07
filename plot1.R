4ab60691b56d166933c0bda7c328cb125e71b562
Verinn <atobe@vip.qq.com>
2015-06-08 01:42:30 +0800

    Delete plot1.R

--- plot1.R
+++
@@ -1,4 +0,0 @@
-rawdata<-read.table("household_power_consumption.txt",header=TRUE,sep=";",na.strings="?",as.is=TRUE)
-rawdata<-subset(rawdata,Date=="1/2/2007"|Date=="2/2/2007")
-png(filename="plot1.png",width=480,height=480)
-hist(rawdata$Global_active_power,xlab="Global Active Power (kilowatts)",ylab="Frequency",main="Global Active Power",col="red")
